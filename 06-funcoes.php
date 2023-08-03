53b5fd032643357f3fa891acda86d243a86b237c
Eliel Moura <eliellpf@gmail.com>
2023-08-03 10:41:10 -0300

    early return

--- 06-funcoes.php
+++ 06-funcoes.php
@@ -93,16 +93,19 @@
         dos dados manipulados pela função, além de melhorar
         as mensagens de erro quando ocorrem problemas.*/
         function verificaNegativo(int $valor):string{
+            /*Early return
+            (é possível omitir o else neste caso) */
             if($valor < 0){
                 return "é negativo";
-            } else {
+            } 
                 return "não é negativo";
             }
-        }
+        
+        // Código abaixo da erro ao usar indução de tipos:
+        /*<p>Número 50: <?=verificaNegativo("texto")?> </p>*/
         ?>
 
         <p>Número 10: <?=verificaNegativo(10)?> </p>
         <p>Número -10: <?=verificaNegativo(-10)?> </p>
-        <p>Número 50: <?=verificaNegativo("texto")?> </p>
 </body>
-</html>fi
+</html>

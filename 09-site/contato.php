8b92381cee6ead3116dbf47e8eeeffe9f9b68109
Eliel Moura <eliellpf@gmail.com>
2023-08-08 09:00:46 -0300

    resolvendo o exercicio

---
+++ 09-site/contato.php
@@ -0,0 +1,8 @@
+<?php
+$pagina = "Contato";
+require "includes/cabecalho.php"
+?>
+
+<h2>Entre em contato</h2>
+<p>Em breve</p>
+<?php require "includes/rodape.php"?>
--- 09-site/includes/rodape.php
+++ 09-site/includes/rodape.php
@@ -1,7 +1,10 @@
 </main>
-
+<?php 
+date_default_timezone_set("America/Sao_Paulo");
+$dataHrAtual = date("d/m/y h:i");
+?>
     <footer>
-        <p>Site criado para o curso <b>Téc. Informática para Internet</b></p>
+        <p>Site criado para o curso <b>Téc. Informática para Internet</b> <br> <time><?=$dataHrAtual?> </time> </p>
     </footer>
 </body>
 </html>
